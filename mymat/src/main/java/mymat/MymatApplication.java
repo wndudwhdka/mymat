2ef2ff678b0af35868cf55c5833ad4099b39e4cc
wndudwhdka <wndudwhdka@naver.com>
2023-08-16 14:41:55 +0900

    첫커밋

---
+++ mymat/src/main/java/mymat/MymatApplication.java
@@ -0,0 +1,13 @@
+package mymat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymatApplication.class, args);
+	}
+
+}
---
+++ mymat/src/test/java/mymat/MymatApplicationTests.java
@@ -0,0 +1,13 @@
+package mymat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
